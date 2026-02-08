b4aaf45cfd8d392000d063ed3d01f879aca49f8f
Branko Mihajlovic <161602693+BrankoMih@users.noreply.github.com>
2026-02-08 18:55:31 +0100

    uradjen frontend bez backenda, dodat navbar i uradjena navigacija

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Challenges from "./pages/Challenges";
 
-export default function App(){
-  const apiCall = async ()=>{
-    const data = await fetch("http://localhost:3000/");
+export default function App() {
+  const [user, setUser]= useState("");
 
-    console.log(data);
-  }
-  return (
-    <div className="App">
-      <button onClick={apiCall}>Make API Call</button>
+return (
+    <div className="min-h-screen bg-slate-50 text-slate-900">
+      <Navbar user={user} setUser={setUser} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route 
+          path="/challenges" 
+          element={user ? <Challenges /> : <Navigate to="/login" />} 
+        />
+      </Routes>
     </div>
-  )
+  );
 }
