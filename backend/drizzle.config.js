ebc039e4618e1c6808918130f4d4855f3f86b779
Branko Mihajlovic <161602693+BrankoMih@users.noreply.github.com>
2026-02-08 21:35:05 +0100

    azurirao drizzel i drizzel config, sredio docker config file, dodao script za migraciju

--- backend/drizzle.config.js
+++ backend/drizzle.config.js
@@ -1,8 +1,10 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-  schema: "./src/schema/index.js",
-  dialect: 'postgresql',
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/db/schema.js",
+  out: "./drizzle",
+  dialect: "postgresql", // This is the magic line that replaces the ':pg' suffix
   dbCredentials: {
     url: process.env.DB_URL || "postgres://bughunt:bughunt123@localhost:5432/bughunt_db",
   },
-};
+});
