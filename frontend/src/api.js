c40d735de8d2b9d35ce169e543aeaea59f64ca8a
Branko Mihajlovic <161602693+BrankoMih@users.noreply.github.com>
2026-02-24 21:14:23 +0100

    dodate funkcije za primanje i slanje zadataka

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,6 @@
 const API_URL = "http://localhost:5000/api";
 
+
 export const loginUser = async (email, password) => {
   const response = await fetch(`${API_URL}/auth/login`, {
     method: "POST",
@@ -21,6 +22,49 @@ export const fetchChallenges = async () => {
     }
   });
 
+  const data = await response.json();
+  if (!response.ok) throw new Error(data.error);
+  return data;
+};
+
+export const fetchChallangeById = async (id) => {
+  const token = localStorage.getItem("token");
+
+  const response = await fetch(`${API_URL}/challenges/${id}`, {
+    headers: {
+      "Authorization": `Bearer ${token}`
+    }
+  });
+
+  const data = await response.json();
+  if (!response.ok) throw new Error(data.error);
+  return data;
+}
+
+export const submit = async (id, code) => {
+  const token = localStorage.getItem("token");
+
+  const response = await fetch(`${API_URL}/challenges/${id}/submit`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify({ code })
+  });
+  const data = await response.json();
+  if (!response.ok) throw new Error(data.error);
+  return data;
+}
+
+export const fetchSolvedChallengeIds = async () => {
+  const token = localStorage.getItem("token");
+
+  const response = await fetch(`${API_URL}/challenges/solved`, {
+    headers: {
+      "Authorization": `Bearer ${token}`
+    }
+  });
   const data = await response.json();
   if (!response.ok) throw new Error(data.error);
   return data;
