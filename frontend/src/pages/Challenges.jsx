12e4271f12bac909d144f46f322cd90e6de19287
Branko Mihajlovic <161602693+BrankoMih@users.noreply.github.com>
2026-02-24 17:17:09 +0100

    uradjeno zahtevanje zadatka od baze i prikazivanje zadatka dobijeni iz baze

--- frontend/src/pages/Challenges.jsx
+++ frontend/src/pages/Challenges.jsx
@@ -1,24 +1,40 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "../components/Card";
-
+import { Link } from "react-router-dom";
+import { fetchChallenges } from "../api";
+//challenge
 export default function Challenges() {
   const [challenges, setChallenges] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  const loadChallenges = async () => {
+    setLoading(true);
+    const data = await fetchChallenges();
+
+    setChallenges(data);
+  }
   useEffect(() => {
-    const mockData = [
-      { id: 1, title: "The Loop Trap", difficulty: "Easy", description: "This loop never ends. Can you see why?", buggyCode: "for (let i = 0; i >= 0; i++) { ... }" },
-      { id: 2, title: "Null Pointer", difficulty: "Medium", description: "Accessing a property on an undefined object.", buggyCode: "const name = user.profile.name;" },
-      { id: 3, title: "Ghost Equality", difficulty: "Hard", description: "Type coercion is causing issues.", buggyCode: "if (price == true) { ... }" }
-    ];
-    setChallenges(mockData);
+   loadChallenges();
+   setLoading(false);
   }, []);
+
+  if (loading) return <div className="p-8 text-indigo-500 font-mono animate-pulse">{">"} ACCESSING_DATABASE...</div>;
 
   return (
     <div className="p-8 max-w-6xl mx-auto">
       <h2 className="text-3xl font-black mb-8 underline decoration-indigo-500">Bug Gallery</h2>
+
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {challenges.map(c => <Card key={c.id} challenge={c} />)}
       </div>
+      {/* <Link
+        to={`/solve/${challenge.id}`}
+        className="block w-full text-center py-2 bg-indigo-600 hover:bg-indigo-500 text-white text-xs font-bold rounded transition-colors uppercase"
+      >
+        Initialize_Patch
+      </Link> */}
     </div>
-  );
-}
+  )
+};
+
